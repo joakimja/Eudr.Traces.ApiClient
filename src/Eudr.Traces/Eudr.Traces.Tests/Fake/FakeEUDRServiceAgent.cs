230871715a00b4c64c67e597851a7cf28102f097
Joakim Jacobsson <Joakim.Jacobsson@vida.se>
2025-09-30 22:07:36 +0200

    for testing

--- src/Eudr.Traces/Eudr.Traces.Tests/Fake/FakeEUDRServiceAgent.cs
+++ src/Eudr.Traces/Eudr.Traces.Tests/Fake/FakeEUDRServiceAgent.cs
@@ -1,4 +1,5 @@
 ﻿using Eudr.Traces.Integrations;
+using Eudr.Traces.Integrations.Entities;
 using Eudr.Traces.Integrations.ServiceAgents.Proxys.EUDRRetrievalService;
 using Eudr.Traces.Integrations.ServiceAgents.Proxys.EUDRSubmissionService;
 using System;
@@ -9,14 +10,41 @@ using System.Threading.Tasks;
 
 namespace Eudr.Traces.Tests.Fake
 {
-    internal class FakeEUDRServiceAgent : IEUDRServiceAgent
+    public class FakeEUDRServiceAgent : IEUDRServiceAgent
     {
-        public Task<IEnumerable<StatementInfoType>> GetDdsStatusAsync(IEnumerable<string> ddsIdentifiers)
+        private List<StatementInfoType> _statementInfoTypes;
+        public FakeEUDRServiceAgent(List<StatementInfoType> statementInfoTypes)
+        {
+                _statementInfoTypes=statementInfoTypes;
+        }
+        public Task<IEnumerable<StatementInfoType>> GetDdsStatusAsync(IEnumerable<Guid> ddsIdentifiers)
+        {
+            return Task.FromResult(_statementInfoTypes.Where(c => ddsIdentifiers.Select(g => g.ToString()).Contains(c.identifier)));
+           
+        }
+
+      
+        public Task<GetStatementByIdentifiersResponse> GetStatementByIdentifiersAsync(string reference, string verification)
         {
             throw new NotImplementedException();
         }
 
         public Task<SubmitStatementResponse> SubmitDdsAsync(SubmitStatementRequestType request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> SubmitDdsAsync(DDSWithGps request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> SubmitDdsAsync(DDSWithReference request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateDDSAsync(string reference, string verification)
         {
             throw new NotImplementedException();
         }
